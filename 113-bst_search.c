accb044cd2a502751acb2aa89c6c4e1c5b85c4c6
Nakuru Wubni <nwubni@gmail.com>
2023-08-31 14:42:15 +0100

    BST - Search

---
+++ 113-bst_search.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+* bst_search - Searches for value in tree
+* @tree: Pointer to tree root
+* @value: Search value
+* Return: Pointer to value node
+*/
+
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (!tree)
+		return (NULL);
+
+	if (value > tree->n)
+		return bst_search(tree->right, value);
+
+	if (value < tree->n)
+		return bst_search(tree->left, value);
+
+	return ((bst_t *)tree);
+}
