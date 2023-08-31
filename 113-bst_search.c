0b98f192bdf558ef52ad3a3f0d19fbec49a34cd4
Nakuru Wubni <nwubni@gmail.com>
2023-08-31 15:03:08 +0100

    BST - Search

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -13,10 +13,10 @@ bst_t *bst_search(const bst_t *tree, int value)
 		return (NULL);
 
 	if (value > tree->n)
-		return bst_search(tree->right, value);
+		return (bst_search(tree->right, value));
 
 	if (value < tree->n)
-		return bst_search(tree->left, value);
+		return (bst_search(tree->left, value));
 
 	return ((bst_t *)tree);
 }
