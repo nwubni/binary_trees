da8a0b598d73c8c2a089b424d74fb8b6d4b1dd54
Nakuru Wubni <nwubni@gmail.com>
2023-08-31 11:26:11 +0100

    Is BST

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -46,5 +49,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
+
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_search(const bst_t *tree, int value);
 
 #endif
